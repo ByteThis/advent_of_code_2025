763865e5f5a2c304631c58ef4e7cca8f3e66e29f
bytethis <casmiguelf@gmail.com>
2025-12-04 05:09:23 +0000

    day_1

---
+++ day_1.py
@@ -0,0 +1,35 @@
+def part_1():
+    dial = 50
+    zero_counter = 0
+    with open('data/day_1/part_1.txt', 'r') as f:
+        for line in f:
+            rotation = int(line.strip().replace('L','-').replace('R',''))
+            dial += rotation
+
+            if dial%100 == 0:
+                zero_counter += 1
+
+    return zero_counter
+
+def part_2():
+    dial = 50
+    zero_counter = 0
+    with open('data/day_1/part_1.txt', 'r') as f:
+        for line in f:
+            rotation = int(line.strip().replace('L','-').replace('R',''))
+            current_dial = dial + rotation
+
+            if rotation >= 0:
+                passes = current_dial // 100
+            else:
+                passes = ((dial - 1) // 100) - ((current_dial - 1) // 100)
+
+            zero_counter += passes
+            dial = current_dial % 100
+
+    return zero_counter
+
+
+print(part_1())
+print(part_2())
+
